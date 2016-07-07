bbb8f04af30afb4badf285bf765804c086d9c5a1
Gary <garyherd@yahoo.com>
2016-07-06 22:49:53 -0500

    Initial commit

--- js/app.js
+++ js/app.js
@@ -24,11 +24,30 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    // parameters?
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(directionStr) {
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [];
 // Place the player object in a variable called player
+var player = new Player();
 
 
 
